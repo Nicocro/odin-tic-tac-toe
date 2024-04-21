2bbf900bd9eeb6cf9433c0974e605cad302e8dd4
nicocro <ncroce00@gmail.com>
2024-04-21 22:39:55 +0100

    shitty state, need to improve

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
 function gameBoard () {
-    this.board = [
-        [null, null, null]
-        [null, null, null]
-        [null, null, null]
+    board = [
+        [0, 0, 0]
+        [0, 0, 0]
+        [0, 0, 0]
     ]
 }
 
-
-function player (name) {
-    this.name = name
+function Player (name, symbol) {
+    this.name = name; 
+    this.symbol = symbol;
 }
 
-const gameBoard = new gameBoard(); 
+const p1 = new Player('p1', 1);
+const p2 = new Player('p2', 2);
+
+function gameLogic (playerOne, playerTwo) {
+
+    const players = [p1, p2];
+
+    let activePlayer = players[0];
+    
+    const switchTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    }; 
+
+    const getActivePlayer = () => activePlayer;
+
+    const playRound = (x, y, board) {
+        if (board[x][y] == 0){
+            board[x][y] = getActivePlayer().symbol;
+        }
+        switchTurn();
+        return board; // here I need to throw an error or somehting in the else statement
+    }
+
+    const printNewRound () => {
+        console.log(board);
+        console.log(`it is ${activePlayer.name} turn`)
+    }
+
+
+}
