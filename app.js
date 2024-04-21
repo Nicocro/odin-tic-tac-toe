fb491605b4d8538344dd31dd38556cfedfa91c5b
nicocro <ncroce00@gmail.com>
2024-04-21 20:23:18 +0100

    basic skeleton

---
+++ app.js
@@ -0,0 +1,14 @@
+function gameBoard () {
+    this.board = [
+        [null, null, null]
+        [null, null, null]
+        [null, null, null]
+    ]
+}
+
+
+function player (name) {
+    this.name = name
+}
+
+const gameBoard = new gameBoard(); 
